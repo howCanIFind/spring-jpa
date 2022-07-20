f4b125cc490acaa32b24e7987f617fe626520207
howCanIFind <second9000@naver.com>
2022-07-20 15:28:20 +0900

    엔티티 클래스 개발1

---
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Embeddable
+@Getter @Setter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+
+}
