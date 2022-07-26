508e5861e59af9ebc110d2178be8402a90fad2ee
howCanIFind <second9000@naver.com>
2022-07-26 21:37:35 +0900

    상품 엔티티 개발(비즈니스 로직 추가)

--- 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,4 +29,23 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>() ;
 
+    //== 비즈니스 로직 ==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int reStocck = this.stockQuantity - quantity;
+        if (reStocck < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = reStocck;
+    }
 }
