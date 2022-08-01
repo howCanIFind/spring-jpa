2202cdd925a5c5a2309597875dfa33286515b4b7
howCanIFind <second9000@naver.com>
2022-08-01 16:14:49 +0900

    주문 목록 검색, 취소

--- 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -42,7 +42,7 @@ public class MemberController {
         return "redirect:/";
     }
 
-    @PostMapping("/members")
+    @GetMapping("/members")
     public String list(Model model) {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
