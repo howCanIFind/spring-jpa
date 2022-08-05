d7ac5c65d9babced3bf72aac4c83a24f9326c6ad
second9000@naver.com <second9000@naver.com>
2022-08-05 09:04:55 +0900

    간단한 주문 조회 V4: JPA에서 DTO로 바로 조회

--- 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +26,7 @@ import java.util.List;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> orderV1() {
@@ -50,6 +53,10 @@ public class OrderSimpleApiController {
                 .toList();
     }
 
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
 
     @Data
     private static class SimpleOrderDto {
