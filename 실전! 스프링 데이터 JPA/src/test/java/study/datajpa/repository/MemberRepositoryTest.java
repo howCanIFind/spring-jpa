ff33830b2652ed12f06fa2e6dde2e0a4d64b6a4f
second9000@naver.com <second9000@naver.com>
2022-08-17 16:03:34 +0900

    @EntityGraph

--- 실전! 스프링 데이터 JPA/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 실전! 스프링 데이터 JPA/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -229,4 +229,35 @@ class MemberRepositoryTest {
         // then
         assertThat(resultCount).isEqualTo(3);
     }
+
+    @Test
+    public void findMemberLazy() {
+        // given
+        // member1 -> teamA
+        // member2 -> teamB
+
+        Team teamA = new Team("TeamA");
+        Team teamB = new Team("TeamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+
+        em.flush();
+        em.clear();
+
+        // when
+        // select Member
+//        List<Member> members = memberRepository.findAll();
+        List<Member> members = memberRepository.findMemberFetchJoin();
+
+        for (Member member : members) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.teamClass = " + member.getTeam().getClass());
+            System.out.println("member.team = " + member.getTeam().getName());
+        }
+
+
+    }
 }
