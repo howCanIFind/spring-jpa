ea36a99b5a62bcaf7878d54039bfe71ae0986739
second9000@naver.com <second9000@naver.com>
2022-08-18 09:32:47 +0900

    JPA Hint & Lock

--- 실전! 스프링 데이터 JPA/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 실전! 스프링 데이터 JPA/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -257,7 +257,36 @@ class MemberRepositoryTest {
             System.out.println("member.teamClass = " + member.getTeam().getClass());
             System.out.println("member.team = " + member.getTeam().getName());
         }
-
-
     }
+
+    @Test
+    public void queryHint() {
+        // given
+        Member member1 = new Member("member1", 10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findReadOnlyByUsername("member1"); // readOnly 이기 때문에 변경 감지 체크를 하지 않음.
+//        findMember.setUsername("member2");
+
+        em.flush();
+    }
+
+    @Test
+    public void lock() {
+        // given
+        Member member1 = new Member("member1", 10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> result = memberRepository.findLockByUsername("member1");// readOnly 이기 때문에 변경 감지 체크를 하지 않음.
+//        findMember.setUsername("member2");
+
+        em.flush();
+    }
+
 }
