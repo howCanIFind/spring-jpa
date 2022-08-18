6d5399e42daaad9cdd1cafd31bdd6018eeb5976e
second9000@naver.com <second9000@naver.com>
2022-08-18 11:14:12 +0900

    사용자 정의 리포지토리 구현

--- 실전! 스프링 데이터 JPA/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 실전! 스프링 데이터 JPA/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -289,4 +289,9 @@ class MemberRepositoryTest {
         em.flush();
     }
 
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
+
 }
