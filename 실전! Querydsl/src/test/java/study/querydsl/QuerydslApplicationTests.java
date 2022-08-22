61110845c3d3103ba023075dc875424f82290c44
second9000@naver.com <second9000@naver.com>
2022-08-22 13:14:09 +0900

    시작 - JPQL vs Querydsl

--- 실전! Querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ 실전! Querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -5,34 +5,29 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
-
 import javax.persistence.EntityManager;
-
-import static org.assertj.core.api.Assertions.*;
+import java.util.List;
 
 @SpringBootTest
 @Transactional
 class QuerydslApplicationTests {
-
     @Autowired
     EntityManager em;
-
     @Test
     void contextLoads() {
         Hello hello = new Hello();
         em.persist(hello);
-
         JPAQueryFactory query = new JPAQueryFactory(em);
-        QHello qHello = QHello.hello;
-
-        Hello result = query.selectFrom(qHello)
+        QHello qHello = QHello.hello; //Querydsl Q타입 동작 확인
+        Hello result = query
+                .selectFrom(qHello)
                 .fetchOne();
-
-        assertThat(result).isEqualTo(hello);
-        assertThat(result.getId()).isEqualTo(hello.getId());
+        Assertions.assertThat(result).isEqualTo(hello);
+//lombok 동작 확인 (hello.getId())
+        Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
     }
-
 }
